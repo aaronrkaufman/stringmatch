e98113e07133fd09799aca60d6fc7ce32a1b461b
Aaron Kaufman <ak8096@nyu.edu>
2021-10-13 21:00:52 +0400

    add data.r

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Training data
+#'
+#' This is a data frame used to train our ranger model
+#'
+#' @name train
+#' @docType data
+#' @format A training data set of Amicus-cosigning orgs and Bonica's DIME orgs.
+#' @keywords dataset
+"train"
+
+#' Trained model
+#'
+#' A ranger trained model object
+#'
+#' @name m
+#' @docType data
+#' @format A ranger object
+#' @keywords dataset
+"m"

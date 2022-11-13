e11d39639e69c0005ffc8374514afc587b35102e
steffen <steffen.stell@posteo.de>
2022-11-13 16:25:52 +0100

    add single vector option for deduplication

--- R/get_features.R
+++ R/get_features.R
@@ -9,14 +9,24 @@
 
 
 get_features <-
-  function(string1, string2){
-    tmp = expand.grid(string1, string2)
-    tmp$osa = stringdist::stringdist(tmp[,1], tmp[,2], method = "osa")
-    tmp$cosine = stringdist::stringdist(tmp[,1], tmp[,2], method = "cosine")
-    tmp$jaccard = stringdist::stringdist(tmp[,1], tmp[,2], method = "jaccard")
-    tmp$jw = stringdist::stringdist(tmp[,1], tmp[,2], method = "jw")
-    tmp$lcs = stringdist::stringdist(tmp[,1], tmp[,2], method = "lcs")
-    tmp$qgram = stringdist::stringdist(tmp[,1], tmp[,2], method = "qgram")
-    tmp$soundex = stringdist::stringdist(tmp[,1], tmp[,2], method = "soundex")
-    return(tmp)  
+  function(string1, string2 = NULL) {
+    if (is.null(string2)) {
+      tmp = string1 |>
+        length() |>
+        combn(2) |>
+        split(1:2) |>
+        lapply(\(x) string1[x]) |>
+        as.data.frame() |>
+        setNames(c("Var1", "Var2"))
+    } else {
+      tmp = expand.grid(string1, string2)
+    }
+    tmp$osa = stringdist::stringdist(tmp[, 1], tmp[, 2], method = "osa")
+    tmp$cosine = stringdist::stringdist(tmp[, 1], tmp[, 2], method = "cosine")
+    tmp$jaccard = stringdist::stringdist(tmp[, 1], tmp[, 2], method = "jaccard")
+    tmp$jw = stringdist::stringdist(tmp[, 1], tmp[, 2], method = "jw")
+    tmp$lcs = stringdist::stringdist(tmp[, 1], tmp[, 2], method = "lcs")
+    tmp$qgram = stringdist::stringdist(tmp[, 1], tmp[, 2], method = "qgram")
+    tmp$soundex = stringdist::stringdist(tmp[, 1], tmp[, 2], method = "soundex")
+    return(tmp)
   }
